c0b43a4b2cdd538cdb8e35770da83ea3961a1f28
Rodr-7 <rodrigo.albornoz11@inacapmail.cl>
2025-06-25 15:15:07 -0400

    Se añade funciones de comprobrobacion de select para habiilitar boton de compra y sorpresita

--- script.js
+++ script.js
@@ -67,22 +67,29 @@ function ValidaNombre(nombre) {
 - El $ indica el final de la cadena.*/
 }
 
+function ValidaDireccion(direccion) {
+  const regex = /^(?=.*[a-zA-Z0-9])[a-zA-Z0-9\s]+$/; /* puede y debe incluir numeros y letras*/
+  return regex.test(direccion);
+}
+
 //------------- Validacion de email -----------------
 function ValidaEmail(email) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; /*constante que almacena la expresión regular para validar el formato del correo electrónico*/
   return regex.test(email);
 }
 
-  // Código que se ejecuta cuando se envía el formulario
+// Código que se ejecuta cuando se envía el formulario   -------------------------------
 document.getElementById('Formulario').addEventListener('submit', function(event) { //El evento 'submit' ocurre cuando el usuario intenta enviar el formulario (por ejemplo, al hacer clic en un botón de tipo submit).
 
 event.preventDefault(); // Evita que el formulario se envíe y recargue la página
 
 const nombre = document.getElementById('nombre').value; // Obtiene el valor del campo nombre
 const email = document.getElementById('email').value; // Obtiene el valor del campo email
+const direccion = document.getElementById('direccion').value; // Obtiene el valor del campo direccion
+
 const validNombre = document.getElementById('NombreValid');   // Obtiene el elemento donde se mostrará el mensaje
 const validEmail = document.getElementById('EmailValid');   // Obtiene el elemento donde se mostrará el mensaje
-const validDireccion = document.getElementById('DirecciónValid');   // Obtiene el elemento donde se mostrará el mensaje
+const validDireccion = document.getElementById('DireccionValid');   // Obtiene el elemento donde se mostrará el mensaje
 
 
 if (ValidaEmail(email)) { // Si el email es válido
@@ -100,4 +107,45 @@ if (ValidaNombre(nombre)) {
     validNombre.textContent = "Nombre inválido"; // Muestra mensaje de error para nombre
     validNombre.style.color = "red";            // Cambia el color del mensaje a rojo
 }
+
+if (ValidaDireccion(direccion)) {
+    validDireccion.textContent = "Direccion válida";   // Muestra mensaje de éxito para nombre
+    validDireccion.style.color = "green";          // Cambia el color del mensaje a verde
+} else { // Si el nombre no es válido
+    validDireccion.textContent = "Direccion inválida"; // Muestra mensaje de error para nombre
+    validDireccion.style.color = "red";            // Cambia el color del mensaje a rojo
+}
+
+});
+
+
+// Código que se ejecuta cuando se selecciona    -------------------------------
+function verificarSelects() {
+  const placa = document.getElementById('placa-madre').value;
+  const procesador = document.getElementById('procesador').value;
+  const ram = document.getElementById('ram').value;
+  const grafica = document.getElementById('grafica').value;
+  const boton = document.getElementById('botonCarrito');
+
+  const nombre = document.getElementById('nombre').value; // Obtiene el valor del campo nombre
+  const email = document.getElementById('email').value; // Obtiene el valor del campo email
+  const direccion = document.getElementById('direccion').value; // Obtiene el valor del campo direccion
+
+  if (placa !== "0" && procesador !== "0" && ram !== "0" && grafica !== "0" && ValidaEmail(email) && ValidaNombre(nombre) && ValidaDireccion(direccion)) {
+    boton.style.display = "inline-block";
+  } else {
+    boton.style.display = "none";
+  }
+}
+
+// Agrega el listener a cada select
+document.getElementById('placa-madre').addEventListener('change', verificarSelects);
+document.getElementById('procesador').addEventListener('change', verificarSelects);
+document.getElementById('ram').addEventListener('change', verificarSelects);
+document.getElementById('grafica').addEventListener('change', verificarSelects);
+
+//------------- Mostrar video --------------------------------------------------
+document.getElementById('botonCarrito').addEventListener('click', function() {
+  document.getElementById('video').classList.remove('oculto');
+  video.play();
 });
